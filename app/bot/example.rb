96b42912a8f8e323afa15c94715c3340772fd778
Mc Dave Valdecantos <mdvaldecantos@gmail.com>
2017-09-24 01:12:28 +0800

    add case test to bot

--- app/bot/example.rb
+++ app/bot/example.rb
@@ -1,5 +1,11 @@
 include Facebook::Messenger
 
 Bot.on :message do |message|
-  message.reply(text: 'Hello, human!')
+  text = case message.text
+         when 'Mc Dave' then 'Oops, thats private i cannot tell anyone about my creator.'
+         else
+           'Im, sorry i just dont know how to response.'
+         end
+
+  message.reply(text: text)
 end
