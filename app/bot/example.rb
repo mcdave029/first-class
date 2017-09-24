9bc6d68ea7e288af186bdc543c23ecc9c12beef9
Mc Dave Valdecantos <mdvaldecantos@gmail.com>
2017-09-24 14:10:36 +0800

    use second of array and move find text to downcase

--- app/bot/example.rb
+++ app/bot/example.rb
@@ -4,14 +4,14 @@ Bot.on :message do |message|
   arr_string = message.text.downcase.split(';')
   able_to_teach = arr_string.count == 2 && arr_string[0]&.include?('keyword:') && arr_string[1]&.include?('reply:')
   if able_to_teach
-    response = Response.new(keyword: arr_string[0].split(':')[0], reply: arr_string[1].split(':')[0])
+    response = Response.new(keyword: arr_string[0].split(':')[1], reply: arr_string[1].split(':')[1])
     text = if response.save
              "Got it thanks. now try #{response.keyword} to see if its working. :D"
            else
              "Im sorry but #{response.errors.full_messages.to_sentence}"
            end
   else
-    response = Response.where('lower(keyword) = ?', message.text).first
+    response = Response.where('lower(keyword) = ?', message.text.downcase).first
     text = if response 
              response.reply
            else
