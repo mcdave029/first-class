6ac2ebaf9c083da7b0b8686becd41b62e4c1bc8b
Mc Dave Valdecantos <mdvaldecantos@gmail.com>
2017-09-24 14:03:39 +0800

    add case insensitive search

--- app/bot/example.rb
+++ app/bot/example.rb
@@ -1,17 +1,17 @@
 include Facebook::Messenger
 
 Bot.on :message do |message|
-  arr_string = message.text.split(';')
+  arr_string = message.text.downcase.split(';')
   able_to_teach = arr_string.count == 2 && arr_string[0]&.include?('keyword:') && arr_string[1]&.include?('reply:')
   if able_to_teach
-    response = Response.new(keyword: arr_string[0], reply: arr_string[1])
+    response = Response.new(keyword: arr_string[0].split(':')[0], reply: arr_string[1].split(':')[0])
     text = if response.save
              "Got it thanks. now try #{response.keyword} to see if its working. :D"
            else
              "Im sorry but #{response.errors.full_messages.to_sentence}"
            end
   else
-    response = Response.find_by(keyword: message.text)
+    response = Response.where('lower(keyword) = ?', message.text).first
     text = if response 
              response.reply
            else
