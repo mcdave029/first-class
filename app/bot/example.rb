721bb16744125eb19d9d2883a79bf2b2ea563f6d
Mc Dave Valdecantos <mdvaldecantos@gmail.com>
2017-09-24 13:55:44 +0800

    enable teach

--- app/bot/example.rb
+++ app/bot/example.rb
@@ -1,12 +1,22 @@
 include Facebook::Messenger
 
 Bot.on :message do |message|
-  response = Response.find_by(keyword: message.text)
-  text = if response 
-           response.reply
-         else
-           'Im sorry i cannot proccess what did you say. :('
-         end
-
+  arr_string = message.text.split(';')
+  able_to_teach = arr_string.count == 2 && arr_string[0]&.include?('keyword:') && arr_string[1]&.include?('reply:')
+  if able_to_teach
+    response = Response.new(keyword: arr_string[0], reply: arr_string[1])
+    text = if response.save
+             "Got it thanks. now try #{response.keyword} to see if its working. :D"
+           else
+             "Im sorry but #{response.errors.full_messages.to_sentence}"
+           end
+  else
+    response = Response.find_by(keyword: message.text)
+    text = if response 
+             response.reply
+           else
+             'Im sorry i cannot proccess what did you say. :('
+           end
+  end
   message.reply(text: text)
 end
--- app/models/response.rb
+++ app/models/response.rb
@@ -1,2 +1,4 @@
 class Response < ApplicationRecord
+  validates :keyword, uniqueness: { case_sensitive: false }
+  validates :keyword, uniqueness: { scope: :reply, message: 'and reply are invalid' }
 end
